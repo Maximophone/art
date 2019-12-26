8c754aafed1d6396c4a01ca1d8bcd5077bf1c258
Maximux <fournes.maxime@gmail.com>
2019-12-26 22:15:13 +0000

    Art backup

--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@ git add -u
 git add --all *.aseprite
 git add --all *.pyxel
 git add --all *.png
+git add --all *.tmx *.tsx
 
 git ci -m "Art backup"
 
---
+++ nature_tileset.tsx
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="nature_tileset" tilewidth="64" tileheight="64" tilecount="184" columns="8">
+ <image source="nature_tileset.png" width="512" height="1472"/>
+ <terraintypes>
+  <terrain name="grass" tile="14"/>
+  <terrain name="under_ground" tile="65"/>
+  <terrain name="walls" tile="117"/>
+  <terrain name="hole" tile="141"/>
+  <terrain name="water" tile="160"/>
+ </terraintypes>
+ <tile id="5" terrain=",,,0"/>
+ <tile id="6" terrain=",,0,0"/>
+ <tile id="7" terrain=",,0,"/>
+ <tile id="13" terrain=",0,,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="0,,0,"/>
+ <tile id="21" terrain=",0,,"/>
+ <tile id="22" terrain="0,0,,"/>
+ <tile id="23" terrain="0,,,"/>
+ <tile id="46" terrain=",,,1"/>
+ <tile id="47" terrain=",,1,1"/>
+ <tile id="48" terrain=",,1,"/>
+ <tile id="49" terrain="1,1,1,"/>
+ <tile id="50" terrain="1,1,,1"/>
+ <tile id="54" terrain=",1,,1"/>
+ <tile id="55" terrain="1,1,1,1"/>
+ <tile id="56" terrain="1,,1,"/>
+ <tile id="57" terrain="1,,1,1"/>
+ <tile id="58" terrain=",1,1,1"/>
+ <tile id="62" terrain=",1,,"/>
+ <tile id="63" terrain="1,1,,"/>
+ <tile id="64" terrain="1,,,"/>
+ <tile id="80" terrain="0,0,0,"/>
+ <tile id="81" terrain="0,0,,0"/>
+ <tile id="88" terrain="0,,0,0"/>
+ <tile id="89" terrain=",0,0,0"/>
+ <tile id="104" terrain=",,,2"/>
+ <tile id="105" terrain=",,2,2"/>
+ <tile id="106" terrain=",,2,"/>
+ <tile id="112" terrain=",2,,2"/>
+ <tile id="113" terrain="2,2,2,2"/>
+ <tile id="114" terrain="2,,2,"/>
+ <tile id="115" terrain="2,2,2,"/>
+ <tile id="116" terrain="2,2,,2"/>
+ <tile id="120" terrain=",2,,"/>
+ <tile id="121" terrain="2,2,,"/>
+ <tile id="122" terrain="2,,,"/>
+ <tile id="123" terrain="2,,2,2"/>
+ <tile id="124" terrain=",2,2,2"/>
+ <tile id="136" terrain=",,,3"/>
+ <tile id="137" terrain=",,3,3"/>
+ <tile id="138" terrain=",,3,"/>
+ <tile id="139" terrain="3,3,3,"/>
+ <tile id="140" terrain="3,3,,3"/>
+ <tile id="144" terrain=",3,,3"/>
+ <tile id="145" terrain="3,3,3,3"/>
+ <tile id="146" terrain="3,,3,"/>
+ <tile id="147" terrain="3,,3,3"/>
+ <tile id="148" terrain=",3,3,3"/>
+ <tile id="152" terrain=",3,,"/>
+ <tile id="153" terrain="3,3,,"/>
+ <tile id="154" terrain="3,,,"/>
+ <tile id="155" terrain=",,,4"/>
+ <tile id="156" terrain=",,4,4"/>
+ <tile id="157" terrain=",,4,"/>
+ <tile id="161" terrain="4,4,4,"/>
+ <tile id="162" terrain="4,4,,4"/>
+ <tile id="163" terrain=",4,,4"/>
+ <tile id="164" terrain="4,4,4,4"/>
+ <tile id="165" terrain="4,,4,"/>
+ <tile id="169" terrain="4,,4,4"/>
+ <tile id="170" terrain=",4,4,4"/>
+ <tile id="171" terrain=",4,,"/>
+ <tile id="172" terrain="4,4,,"/>
+ <tile id="173" terrain="4,,,"/>
+ <wangsets>
+  <wangset name="cliff" tile="-1">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="32" wangid="0x11111211"/>
+   <wangtile tileid="33" wangid="0x12111211"/>
+   <wangtile tileid="34" wangid="0x12111111"/>
+  </wangset>
+ </wangsets>
+</tileset>
